73cb690abee95360e1b47cea89d3d97a8e4ba4b5
João R. M. Nicola <joaoraf@gmail.com>
2026-04-28 16:59:57 -0300

    Correção de bug no reconhecimento de complemento em rótulo de Alínea (#248857)

--- src/main/scala/br/gov/lexml/parser/pl/rotulo/rotuloParser.scala
+++ src/main/scala/br/gov/lexml/parser/pl/rotulo/rotuloParser.scala
@@ -77,7 +77,7 @@ object rotuloParser {
 		lazy val lowerAlpha: Parser[Char] = elem("Alpha", c => c >= 'a' && c <= 'z')
 		lazy val complemento: Parser[Int] = hyphenOrSimilar ~> (lowerAlpha +) <~ guard(fimComplemento) ^^ complementoToInteger
 
-		lazy val fimComplemento: Parser[Unit] = (elem("fimComplemento", c => ",\\.;: ".contains(c)) ^^^ ((): Unit)) | eos
+		lazy val fimComplemento: Parser[Unit] = (elem("fimComplemento", c => ",\\.;: )".contains(c)) ^^^ ((): Unit)) | eos
 
 		def ordinalExtenso(g: Genero): Parser[(Int, Boolean)] =
 			("unic" ^^^ (1, true) | "primeir" ^^^ (1, false) | "segund" ^^^ (2, false) | "terceir" ^^^ (3, false)
